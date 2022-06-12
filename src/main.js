9ba9e3174cf3904c86df1a2b804824f85ccb0495
NataliaVozhdaeva <LomzNat@yandex.ru>
2022-06-12 16:41:58 +0200

    new comment state

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { render } from './framework/render.js';
 //import FilterView from './view/filterView';
 import UserSectionView from './view/userView';
-import BoardFilmsPresenter from './presenter/boardFilmsPresenter.js';
+import FilmsBoardPresenter from './presenter/filmsBoardPresenter';
 import FilmsModel from './model/filmsModel.js';
 //import { generateFilter } from './mock/mockForFilter';
 
@@ -9,10 +9,10 @@ const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 
 const filmsModel = new FilmsModel();
-const boardFilmsPresentor = new BoardFilmsPresenter(siteMainElement, filmsModel);
+const filmsBoardPresenter = new FilmsBoardPresenter(siteMainElement, filmsModel);
 //const filters = generateFilter(filmsModel.films.userDetails);
 
 render(new UserSectionView(), siteHeaderElement);
 //render(new FilterView(filters), siteMainElement);
 
-boardFilmsPresentor.show();
+filmsBoardPresenter.show();
